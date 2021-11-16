5813eaf9d54da217bc1c95743a3ff6a1790d3aab
Opilane TTHK <opilane@tthk.ee>
2021-11-16 15:47:46 +0200

    sitt

---
+++ ChucknorrisRandomjoke/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorrisRandomJoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string filepath = @"C:\Users\opilane\samples\chuckjokes.txt";
+            string[] jokesAboutChuck = File.ReadAllLines(filepath);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, jokesAboutChuck.Length);
+            Console.WriteLine(jokesAboutChuck[randomIndex]);
+            */
+            Console.WriteLine(GetRandomJoke());
+        }
+        public static string GetRandomJoke()
+        {
+            string filepath = @"C:\Users\opilane\samples\chuckjokes.txt";
+            string[] jokesAboutChuck = File.ReadAllLines(filepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, jokesAboutChuck.Length);
+
+            return jokesAboutChuck[randomIndex];
+        }
+
+    }
+}
